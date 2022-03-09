4b555820add904da508ff219d005e35efb7126c5
jenskevin <kevinsitop@hotmail.es>
2022-03-08 20:07:16 -0500

    Mi Commit

---
+++ taller-maven-git/src/main/java/com/estrategit/curso/Persona.java
@@ -0,0 +1,19 @@
+package com.estrategit.curso;
+
+/*
+ * @Autor: JENS WONG
+ * @AÑO: 2022
+ * @DESCRIPCION: CLASE DE PRUEBAS TALLER
+ */
+
+public class Persona {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("HOLA MUNDO");
+	    
+	}
+	
+}
